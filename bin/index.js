7d2e1a6f016ebc2c535b7d2268703df8ab267258
Pavel Piha <ppiha@infoblox.com>
2021-06-17 22:04:35 +0300

    fix: bugs, add confirmation

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,8 @@
 
 const yargs = require("yargs");
 const fs = require("fs");
-const { number } = require("yargs");
+var readlineSync = require("readline-sync");
+
 yargs
   .showHelpOnFail()
   .version()
@@ -15,33 +16,34 @@ const options = yargs
   .usage("Usage: -d <depth> -f <old-file-name> -n <new-file-name> ")
   .option("d", {
     alias: "depth",
-    describe: "depth to search",
+    describe: "depth for search",
     type: "number",
   })
   .option("f", {
     alias: "old-file-name",
-    describe: "New file name",
+    describe: "Old name",
     type: "string",
   })
   .option("n", {
     alias: "new-file-name",
-    describe: "Your name",
+    describe: "New name",
     type: "string",
   })
   .example(
-    "$0 -d 5 -f file1 -n file2",
-    "rename all files containing 'file1' into 'file2' in 5 dirs deep"
+    "farf -d 5 -f old -n new",
+    "Change all files and directories containing 'old' string in the name into 'new' in 5 directories deep"
   )
   .example(
-    "$0 5 file1 file2",
-    "rename all files containing 'file1' into 'file2' in 5 dirs deep"
+    "farf 5 old new",
+    "Change all files and directories containing 'old' string in the name into 'new' in 5 directories deep"
   ).argv;
 
 const [, , depth, oldFileName, newFileName] = process.argv;
 
-const corePath = "./examples";
+const corePath = "./";
 let currentPath = "";
 let currentDepth = 0;
+
 const checkIfFileContainsSubName = (fileName) => fileName.includes(oldFileName);
 
 const goThroughEachPath = (upperPath, paths, currentDepth, maxDepth) => {
@@ -49,7 +51,7 @@ const goThroughEachPath = (upperPath, paths, currentDepth, maxDepth) => {
     const fileContainsSubName = checkIfFileContainsSubName(path);
     let newPath = path;
     let oldLongPath = upperPath.concat("/", path);
-    if (fileContainsSubName === true) {
+    if (fileContainsSubName) {
       newPath = path.replace(oldFileName, newFileName);
       let newLongPath = upperPath.concat("/", newPath);
       fs.renameSync(oldLongPath, newLongPath);
@@ -62,33 +64,74 @@ const goThroughEachPath = (upperPath, paths, currentDepth, maxDepth) => {
 };
 
 const readDir = (path, depth, maxDepth) => {
-  if (path) {
-    currentPath = currentPath.concat("/", path);
-  } else {
-    currentPath = corePath;
-  }
+  currentPath = path ? currentPath.concat("/", path) : corePath;
   currentDepth = depth;
   if (typeof +maxDepth === "number" && currentDepth <= maxDepth) {
     currentDepth++;
-    const paths = fs.readdirSync(currentPath);
+    let paths = [];
+    try {
+      paths = fs.readdirSync(currentPath);
+    } catch (err) {
+      process.stdout.write(
+        `\n Renaming failed!\n Error: no such file or directory\n`
+      );
+      process.exit();
+    }
     goThroughEachPath(currentPath, paths, currentDepth, maxDepth);
   }
+};
+
+const getConfirmation = () => {
+  let answer = readlineSync.question(
+    " Are you sure to continue with such depth? (YES/NO): "
+  );
+  answer = answer.toString().trim().toLocaleLowerCase();
+  if (answer === "yes" || answer === "y") {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const checkSearchDepth = () => {
+  if (depth > 10 && depth <= 15) {
+    return getConfirmation();
+  } else if (depth > 15) {
+    process.stdout.write(" The depth is too big. Max value is 15\n");
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const runRenaming = () => {
+  setTimeout(() => {
+    let isDepthCorrect = checkSearchDepth();
+    if (isDepthCorrect) {
+      let promise = new Promise((resolve) => {
+        setTimeout(() => {
+          readDir("", 0, depth);
+          resolve();
+        }, 0);
+      });
+      promise
+        .then(() => {
+          process.stdout.write("\n Renaming finished\n");
+          process.exit();
+        })
+        .catch((error) => {
+          process.stdout.write(`\n Renaming failed!\n ${error}`);
+        });
+    }
+  }, 0);
 };
 
 if (
   (options.f && options.d && options.n) ||
   (depth && oldFileName && newFileName)
 ) {
-  readDir("", 0, depth);
+  runRenaming(depth);
 } else {
   yargs.showHelp();
+  process.exit();
 }
-
-process
-  .on("unhandledRejection", (reason, p) => {
-    console.error(reason, "Unhandled Rejection at Promise", p);
-  })
-  .on("uncaughtException", (err) => {
-    console.error(err, "Uncaught Exception thrown");
-    process.exit(1);
-  });
